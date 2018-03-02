a660722f4d256a52ed6cdc7b023101c3e1034093
xiaodong <17610278890@163.com>
2018-03-02 15:12:25 +0800

    都是

--- ssm/src/cn/itcast/core/controller/UserController.java
+++ ssm/src/cn/itcast/core/controller/UserController.java
@@ -3,7 +3,6 @@ package cn.itcast.core.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import cn.itcast.core.pojo.User;
 import cn.itcast.core.service.UserService;
 
